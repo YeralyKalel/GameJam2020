3a887a0ad366cd1bbb3fb90def81b63cbcd5a516
Olliescottie2002 <olliescottie2002@gmail.com>
2020-07-11 20:00:12 +0100

    Added wave system

---
+++ Assets/Spawnpoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnpoint : MonoBehaviour
+{
+    private bool currentlySpawning = false;
+    private int enemiesToSpawn;
+
+    public GameObject enemy;
+
+    private void Update()
+    {
+        if (currentlySpawning)
+        {
+            if(enemiesToSpawn <= 0)
+            {
+                CancelInvoke();
+                currentlySpawning = false;
+            }
+        }
+    }
+
+
+
+    public void Spawn(int currentWave)
+    {
+        currentlySpawning = true;
+        enemiesToSpawn = Random.Range(currentWave, currentWave * 2);
+
+        InvokeRepeating("SpawnEnemy", 0.5f, 2.3f);
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemy, transform.position, transform.rotation, EnemyManager.instance.transform);
+        enemiesToSpawn--;
+    }
+}
---
+++ Assets/WaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public float timeBtwWaves;
+    private float currentTime;
+
+    int currentWave = 1;
+
+    private void Update()
+    {
+        if(currentTime <= 0)
+        {
+            //spawn new wave of enemies
+
+            Transform spawnPointParent = transform.GetChild(0);
+
+            for (int p = 0; p < spawnPointParent.childCount; p++)
+            {
+                spawnPointParent.GetChild(p).GetComponent<Spawnpoint>().Spawn(currentWave);
+            }
+
+            currentWave ++;
+            currentTime = timeBtwWaves;
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+        }
+    }
+}
