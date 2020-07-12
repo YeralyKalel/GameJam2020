0a27fb6fae84d290ede4841990c5439b988035b8
YeralyKalel <yeraly.kalel@gmail.com>
2020-07-12 20:06:44 +0600

    Bug Fix

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -52,6 +52,7 @@ public class GameManager : MonoBehaviour
     {
         foreach (EnemyController e in enemyControllers)
         {
+            if (e.gameObject == null) continue;
             Destroy(e.gameObject);
         }
         enemyControllers = new List<EnemyController>();
